5268a0782f23a251424feea1081567b99d236165
Abror <70596977+Abror4544@users.noreply.github.com>
2020-11-14 16:10:32 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,21 @@
+$(function () {
+
+    $('.mainmenu').hide();
+    $('.mainmenuAphorism').hide();
+
+
+    $('.mainmenuBtn').click(function () {
+        $('.mainmenu').fadeIn(500);
+        $('.mainmenuTitle').hide(500);
+        $('.mainmenuAphorism').fadeIn(500);
+        $(this).hide();
+    })
+
+    $('.hideMenu').click(function () {
+        $('.mainmenu').fadeOut(500);
+        $('.mainmenuTitle').fadeIn(500);
+        $('.mainmenuAphorism').fadeOut(500);
+        $('.mainmenuBtn').show();
+    })
+
+})
