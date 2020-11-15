701e9b408db93fe2c462bc0ef71136571cbba507
Abror <70596977+Abror4544@users.noreply.github.com>
2020-11-15 19:25:07 +0500

    Add files via upload

---
+++ portfolio/js/script.js
@@ -0,0 +1,22 @@
+$(function () {
+
+    $('.mainmenu').hide();
+    $('.mainmenuAphorism').hide();
+
+
+    $('.mainmenuBtn').click(function () {
+        $('.mainmenu').fadeIn(500);
+        $('.mainmenuTitle').hide(500);
+        $('.mainmenuAphorism').fadeIn(500);
+        $(this).hide();
+    })
+
+    $('.hideMenu').click(function (event) {
+        event.preventDefault();
+        $('.mainmenu').fadeOut(500);
+        $('.mainmenuTitle').fadeIn(500);
+        $('.mainmenuAphorism').fadeOut(500);
+        $('.mainmenuBtn').show();
+    })
+
+})
